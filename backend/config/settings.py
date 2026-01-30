47562bc4f7e20e281fc24f903ef3d70595c8cfe3
Adam Stone <adamstone.io@protonmail.com>
2026-01-31 06:21:21 +1000

    chore(config): add DATABASE_URL support
        - add dj-database-url and update database configuration
        - refresh backend requirements after install
        - remove accidental requirements.txt in root dir

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,6 +3,8 @@ from datetime import timedelta
 import os
 from django.core.exceptions import ImproperlyConfigured
 from dotenv import load_dotenv
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,10 +82,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
